8efe11bf30a842f8a80619f72b866e9760a2e0d8
Clement Bouakkaz <clementbouakkaz@gmail.com>
2022-06-15 00:14:17 +0200

    nuits lumiere page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :parcours, :tarifs, :contact, :cgu, :mentions_legales, :politique_confidentialite ]
+  skip_before_action :authenticate_user!, only: [ :home, :parcours, :tarifs, :nuitslumiere, :contact, :cgu, :mentions_legales, :politique_confidentialite ]
 
   def home
   end
@@ -8,6 +8,9 @@ class PagesController < ApplicationController
   end
 
   def tarifs
+  end
+
+  def nuitslumiere
   end
 
   def contact
