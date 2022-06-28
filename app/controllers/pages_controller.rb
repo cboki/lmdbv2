6de8b39d78e24d027996de2ce01dac65be57bfc6
Clement Bouakkaz <clementbouakkaz@gmail.com>
2022-06-28 12:56:07 +0200

    page A vaillant coeur

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :parcours, :tarifs, :reservation, :nuitslumiere, :faq, :contact, :cgu, :mentions_legales, :politique_confidentialite ]
+  skip_before_action :authenticate_user!, only: [ :home, :parcours, :a_vaillant_coeur, :tarifs, :reservation, :nuitslumiere, :faq, :contact, :cgu, :mentions_legales, :politique_confidentialite ]
 
   def home
   end
 
   def parcours
+  end
+
+  def a_vaillant_coeur
   end
 
   def tarifs
