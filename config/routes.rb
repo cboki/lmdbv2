8d10bfb1df3721c033ed4c69703158da47e379fa
Clement Bouakkaz <clementbouakkaz@gmail.com>
2022-07-19 11:27:55 +0200

    ajout page horaires

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get "/parcours/tarifs", to: "pages#tarifs", as: :tarifs
   get "/parcours/a-vaillant-coeur", to: "pages#a_vaillant_coeur", as: :a_vaillant_coeur
   get "/parcours/la-grande-history", to: "pages#la_grande_history", as: :la_grande_history
+  get "/parcours/horaires", to: "pages#horaires", as: :horaires
   get "/reservation", to: "pages#reservation", as: :reservation
   get "/wine-gaming", to: "pages#wine_gaming", as: :wine_gaming
   get "/contact", to: "pages#contact", as: :contact
